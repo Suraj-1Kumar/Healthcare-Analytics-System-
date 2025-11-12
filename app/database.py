07b67386437c185be6427038ce6c684a3974670f
Suraj-Btech <surajkumarsharma516@gmail.com>
2025-11-12 12:12:36 +0530

    Switch to SQLite for production deployment

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+'''from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,4 +9,19 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # This is the line to check carefully!
+Base = declarative_base()
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
